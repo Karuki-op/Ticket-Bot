b38002c239e11b1faeb4f4c81ecf57a62dfa5b21
Karuki <avishyantaryan@gmail.com>
2025-05-22 09:13:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -121,4 +121,4 @@ async def ticket_panel(interaction: discord.Interaction):
     await interaction.channel.send(embed=embed, view=TicketView())
     await interaction.response.send_message("🎫 Ticket panel sent!", ephemeral=True)
 
-bot.run(os.getenv("TOKEN"))
+bot.run(os.getenv("MTM3NDcwNjEzMTE5MjExOTMxNg.Gnvgpn.mBbPSb9TvoAFcxFDeK0VmmqSWouAlkXiSVfiR0"))
